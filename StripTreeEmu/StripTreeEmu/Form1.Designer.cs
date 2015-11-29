7c19ffced4b50f88a9232c0f3a046165a069f207
nlga <gabor.bencsik.regs@gmail.com>
2015-11-30 00:51:10 +0100

    Add-remove point works

--- StripTreeEmu/StripTreeEmu/Form1.Designer.cs
+++ StripTreeEmu/StripTreeEmu/Form1.Designer.cs
@@ -28,22 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.addNewBackgroundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.backgroundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.layoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.layoutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewBackgroundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.addPointsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog_background = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.wtffffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -53,7 +58,7 @@
             this.settingsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(558, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(632, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -66,6 +71,42 @@
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backgroundToolStripMenuItem,
+            this.layoutToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // backgroundToolStripMenuItem
+            // 
+            this.backgroundToolStripMenuItem.Name = "backgroundToolStripMenuItem";
+            this.backgroundToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.backgroundToolStripMenuItem.Text = "Background";
+            this.backgroundToolStripMenuItem.Click += new System.EventHandler(this.backgroundToolStripMenuItem_Click);
+            // 
+            // layoutToolStripMenuItem
+            // 
+            this.layoutToolStripMenuItem.Name = "layoutToolStripMenuItem";
+            this.layoutToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.layoutToolStripMenuItem.Text = "Layout";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.layoutToolStripMenuItem1});
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save As";
+            // 
+            // layoutToolStripMenuItem1
+            // 
+            this.layoutToolStripMenuItem1.Name = "layoutToolStripMenuItem1";
+            this.layoutToolStripMenuItem1.Size = new System.Drawing.Size(110, 22);
+            this.layoutToolStripMenuItem1.Text = "Layout";
             // 
             // exitToolStripMenuItem
             // 
@@ -87,68 +128,58 @@
             this.addNewBackgroundToolStripMenuItem.Name = "addNewBackgroundToolStripMenuItem";
             this.addNewBackgroundToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.addNewBackgroundToolStripMenuItem.Text = "Clear";
-            // 
-            // openToolStripMenuItem
-            // 
-            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.backgroundToolStripMenuItem,
-            this.layoutToolStripMenuItem});
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.openToolStripMenuItem.Text = "Open";
-            // 
-            // backgroundToolStripMenuItem
-            // 
-            this.backgroundToolStripMenuItem.Name = "backgroundToolStripMenuItem";
-            this.backgroundToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.backgroundToolStripMenuItem.Text = "Background";
-            this.backgroundToolStripMenuItem.Click += new System.EventHandler(this.backgroundToolStripMenuItem_Click);
-            // 
-            // layoutToolStripMenuItem
-            // 
-            this.layoutToolStripMenuItem.Name = "layoutToolStripMenuItem";
-            this.layoutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.layoutToolStripMenuItem.Text = "Layout";
-            // 
-            // saveToolStripMenuItem
-            // 
-            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.layoutToolStripMenuItem1});
-            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.saveToolStripMenuItem.Text = "Save As";
-            // 
-            // layoutToolStripMenuItem1
-            // 
-            this.layoutToolStripMenuItem1.Name = "layoutToolStripMenuItem1";
-            this.layoutToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
-            this.layoutToolStripMenuItem1.Text = "Layout";
+            this.addNewBackgroundToolStripMenuItem.Click += new System.EventHandler(this.addNewBackgroundToolStripMenuItem_Click);
             // 
             // addPointsToolStripMenuItem
             // 
             this.addPointsToolStripMenuItem.Name = "addPointsToolStripMenuItem";
             this.addPointsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.addPointsToolStripMenuItem.Text = "Add points";
+            this.addPointsToolStripMenuItem.Click += new System.EventHandler(this.addPointsToolStripMenuItem_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(0, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(234, 195);
+            this.pictureBox1.Size = new System.Drawing.Size(152, 129);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             // 
             // openFileDialog_background
             // 
             this.openFileDialog_background.FileName = "openFileDialog1";
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wtffffToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(108, 48);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening_1);
+            // 
+            // wtffffToolStripMenuItem
+            // 
+            this.wtffffToolStripMenuItem.Enabled = false;
+            this.wtffffToolStripMenuItem.Name = "wtffffToolStripMenuItem";
+            this.wtffffToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.wtffffToolStripMenuItem.Text = "nr";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 431);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(632, 478);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
@@ -156,6 +187,7 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,6 +209,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog_background;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem wtffffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
     }
 }
 
--- StripTreeEmu/StripTreeEmu/Form1.cs
+++ StripTreeEmu/StripTreeEmu/Form1.cs
@@ -10,15 +10,101 @@ using System.Windows.Forms;
 
 namespace StripTreeEmu
 {
+ 
+
     public partial class Form1 : Form
     {
+        private Point MouseDownLocation;
+        private PictureBox currentPB;   // selected by left or right button
+
+        private List<PictureBox> widgetList;
+
+
+        private int getHighestNumberOfList() {
+            int retVal=0;
+            bool found;
+            for (int i = 1; i < Int32.MaxValue ; i++)
+			{
+                found = false;
+                foreach (PictureBox item in widgetList)
+                {
+                    if (i == (int)item.Tag) {
+                        retVal = i;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                    return retVal;
+			}
+            return retVal;
+        }
+
         public Form1()
         {
             InitializeComponent();
             if (System.IO.File.Exists("res/default.jpg"))
                 loadBackground("res/default.jpg");
+
+            widgetList = new List<PictureBox>();
+
+
+
         }
 
+        private void removeAllWidget() {
+            foreach (PictureBox item in widgetList)
+            {
+                item.Hide();
+            }
+            widgetList.Clear();
+        }
+        private void removeWidget(PictureBox pb) {
+            foreach (PictureBox item in widgetList)
+            {
+                if (item == pb)
+                {
+                    widgetList.Remove(item);
+                    pb.Hide();
+                    break;
+                }
+            }
+        }
+
+        private void addWidget(int Ex, int Ey, int tag) {
+            Image img = Image.FromFile("res/point.png" );
+
+            PictureBox pb = new PictureBox();
+            pb.Image = img;
+            pb.Left = Ex-img.Width/2;
+            pb.Top = Ey-img.Height/2;
+            pb.Width = img.Width;
+            pb.Height = img.Height;
+            pb.Parent = pictureBox1;
+            pb.BackColor = Color.Transparent;
+            pb.MouseDown += new System.Windows.Forms.MouseEventHandler(widget_MouseDown);
+            pb.MouseMove += new System.Windows.Forms.MouseEventHandler(widget_MouseMove);
+            pb.Tag = tag;
+            //pb.ContextMenu = contextMenuStrip1;
+            pb.ContextMenuStrip = contextMenuStrip1;
+
+            //write number?
+            if (img.Width >= 20)
+            {
+                Label nr = new Label();
+                nr.Text = tag.ToString();
+                nr.Font = new Font(nr.Font.FontFamily, 6);
+                nr.Parent = pb;
+                nr.Left += 1;
+                nr.Top += 5;
+                nr.MouseDown += new System.Windows.Forms.MouseEventHandler(widget_MouseDown);
+                nr.MouseMove += new System.Windows.Forms.MouseEventHandler(widget_MouseMove);
+                //nr.ContextMenu = contextMenuStrip1;
+                nr.ContextMenuStrip = contextMenuStrip1;
+            }
+            widgetList.Add(pb);
+
+        }
         
         private void loadBackground(string st) {
                 Image image = Image.FromFile(st);
@@ -43,9 +129,63 @@ namespace StripTreeEmu
 
         private void Form1_Resize(object sender, EventArgs e)
         {
-            //Control control = (Control)sender;
-            //pictureBox1.Height = ClientRectangle.Height - 100;
-            //pictureBox1.Width = ClientRectangle.Width - 100;
+
         }
+
+        private void addPointsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            addWidget(100, 100, 1 );
+
+        }
+        private void widget_MouseDown(object sender, MouseEventArgs e)
+        {
+            var pb = sender as PictureBox;
+            
+            if (pb == null)
+                pb = (PictureBox)((Label)sender).Parent;
+
+            currentPB = pb;
+            if ( 
+                (e).Button == System.Windows.Forms.MouseButtons.Left ||
+                (e).Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                MouseDownLocation = e.Location;
+            }
+        }
+        private void widget_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                currentPB.Left = e.X + currentPB.Left - MouseDownLocation.X;
+                currentPB.Top = e.Y + currentPB.Top - MouseDownLocation.Y;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ( (e).Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                addWidget(e.X, e.Y, getHighestNumberOfList() + 1);
+            }
+         
+        }
+
+        private void contextMenuStrip1_Opening_1(object sender, CancelEventArgs e)
+        {
+            ContextMenuStrip strip = (ContextMenuStrip)sender;
+            strip.Items[0].Text = ((int)currentPB.Tag).ToString();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            removeWidget(currentPB);
+        }
+
+        private void addNewBackgroundToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            removeAllWidget();
+        }
+
+
     }
 }
